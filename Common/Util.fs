dd7ab073c0af39cf117290186259f6a6f7be8bfc
hgrsd <daniel@hgrsd.nl>
2022-01-03 14:20:12 +0000

    day3b

--- Common/Util.fs
+++ Common/Util.fs
@@ -1,6 +1,6 @@
 module Common.Util
 
-module Bit = 
-  let getBitValue from idx =
-    let mask = 1 <<< idx
-    if (from &&& mask) <> 0 then 1 else 0
+module Bit =
+  let valueAt length i number =
+    let mask = 1 <<< (length - 1 - i)
+    if (number &&& mask) <> 0 then 1 else 0
--- Day3/Program.fs
+++ Day3/Program.fs
@@ -2,44 +2,63 @@ open System
 open Common.Util
 open Common.Solve
 
-let asInt bits =
-  let mutable x = 0
+let valueAt = Bit.valueAt 12
 
+let toInt bits =
   bits
-  |> List.iteri
-       (fun i bit ->
+  |> List.fold
+       (fun (i, acc) cur ->
          if i = bits.Length - 1 then
-           x <- x ||| bit
+           (i, acc ||| cur)
          else
-           x <- (x ||| bit) <<< 1)
+           (i + 1, (acc ||| cur) <<< 1))
+       (0, 0)
+  |> snd
 
-  x
-
-let asBits number =
-  seq { 0 .. 12 }
-  |> Seq.map (fun i -> Bit.getBitValue number (11 - i))
-  |> List.ofSeq
-
-let computeGamma values =
+let gamma values =
   values
   |> List.fold
        (fun counters bits ->
          counters
-         |> List.mapi (fun i value -> value + Bit.getBitValue value (11 - i)))
-       [ 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0 ]
-  |> List.map (fun x -> Convert.ToInt32(x >= values.Length / 2))
-  |> asInt
+         |> List.mapi (fun i value -> value + valueAt i bits))
+       [ for _ in 0 .. 11 -> 0 ]
+  |> List.map
+       (fun x ->
+         Convert.ToInt32(
+           if values.Length % 2 = 0 then
+             x >= values.Length / 2
+           else
+             x > values.Length / 2
+         ))
+  |> toInt
+
+let epsilon = gamma >> (^^^) 0b111111111111
 
 let part1 parsed =
-  let gamma = computeGamma parsed
-  let epsilon = gamma ^^^ 0b111111111111
-  printfn $"{epsilon * gamma}"
+  printfn $"part1: {epsilon parsed * gamma parsed}"
+
+let rec distill matcher pos inputs =
+  match inputs with
+  | [ x ] -> x
+  | xs ->
+    let expected = valueAt pos (matcher xs)
+
+    distill
+      matcher
+      (pos + 1)
+      (xs
+       |> List.filter (fun value -> valueAt pos value = expected))
+
+let part2 parsed =
+  (distill gamma 0 parsed)
+  * (distill epsilon 0 parsed)
+  |> printfn "part2: %i"
 
 let parse string = Convert.ToInt32(string, 2)
 
 [<EntryPoint>]
 let main _ =
   $"{__SOURCE_DIRECTORY__}/input"
-  |> solve parse [ part1 ]
+  |> solve parse [ part1; part2 ]
 
   0
